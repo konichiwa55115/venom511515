7300850af1850663b6513747c283ea28e110917e
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-03-02 00:06:16 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,3 +4,4 @@ cd venom511515
 pip install -r requirements.txt
 wget https://kopp.iamidiotareyoutoo.com/stream/open.m3u8/BQACAgQAAx0EYtH4LwABB3XfY__L-0wuDWlWy_ybdpxnOxF_S1oAAq8SAALwFwABUHlYhzr-MpsjLgQ/hhh.m4a
 python speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH hhh.m4a trans.txt
+cloudshell dl trans.txt
