9b5b719338f5d9c7d74c18a9247e71da18b8ee3c
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-03-02 00:11:44 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ apt-get install ffmpeg -y
 git clone https://github.com/konichiwa55115/venom511515
 cd venom511515
 pip install -r requirements.txt
-wget https://kopp.iamidiotareyoutoo.com/stream/open.m3u8/BQACAgQAAx0EYtH4LwABB3XfY__L-0wuDWlWy_ybdpxnOxF_S1oAAq8SAALwFwABUHlYhzr-MpsjLgQ/hhh.m4a
-python speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH hhh.m4a trans.txt
+wget https://tg.transloader.workers.dev/6234365091/AgADWw488933
+python speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH AgADWw488933 trans.txt
 cloudshell dl trans.txt
