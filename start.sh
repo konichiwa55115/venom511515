6e5f66e9a3333d1c36298253fa53b39663156c23
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-03-02 00:05:55 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
-
 apt-get install ffmpeg -y
+git clone https://github.com/konichiwa55115/venom511515
+cd venom511515
 pip install -r requirements.txt
+wget https://kopp.iamidiotareyoutoo.com/stream/open.m3u8/BQACAgQAAx0EYtH4LwABB3XfY__L-0wuDWlWy_ybdpxnOxF_S1oAAq8SAALwFwABUHlYhzr-MpsjLgQ/hhh.m4a
+python speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH hhh.m4a trans.txt
