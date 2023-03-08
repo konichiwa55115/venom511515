c85738b4a8e3c2b0ae951f552fd67eb6b27535e6
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-03-08 12:54:17 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-apt-get install ffmpeg -y
-git clone https://github.com/konichiwa55115/venom511515
-cd venom511515
-pip install -r requirements.txt
-wget https://tg.transloader.workers.dev/6234365091/AgADWw488933
-python speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH AgADWw488933 trans.txt
-cloudshell dl trans.txt
